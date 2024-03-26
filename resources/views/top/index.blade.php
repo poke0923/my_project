f2010248d2f36fcd7965adc6be796a9f225851d7
poke0923 <orbis.9482@gmail.com>
2024-03-26 11:50:29 +0900

    Vue基礎3-4章

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -6,6 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title></title>
+
+        <link rel="stylesheet" href="/css/build/top/index.css">
+
     </head>
 
     <body>
@@ -16,8 +19,43 @@
        </header>
 
        <main>
-        <h2 v-html="title"></h2> <!-- v-htmlでデータバインディングしている。イコールの後は表示内容に合わせて、Vue側で設定した名前と同じにする -->
+        <h2 v-html="title"></h2> 
+        <!-- 
+            v-htmlで表示内容についてデータバインディングしている。イコールの後は表示内容に合わせて、Vue側で設定した名前と同じにする
+            v-htmlはjs側でhtmlのタグまで設定するときに使う（例：js側でtitleの値をtitle = "<h2>aaaa</h2>" のようにしてタグまで決めている）
+            v-htmlはユーザーが書き込みを行える内容に設定してしまうと値が書き換わってしまい危険。セキュリティに注意。 
+        -->
         <button v-on:click="buttonClick">変更</button>
+
+        <form action="">
+            <p v-html="validateResult"></p>
+            <input type="text" name="name" v-model="name"> 
+            <!-- 
+                v-modelによりinputタグの入力内容をjs側に紐づけている。
+                v-modelは双方向データバインディングというもので、inputタグの入力値が変わった場合、
+                それをjs側（今回は変数nameの値）にも反映させることができる。
+            -->
+            <button type="button" v-on:click="validate">ひらがな確認</button>
+        </form>
+
+        <section class="tab-layout">
+            <div class="tabs">
+                <button class="tab" v-on:click="page=1">tab1</button> <!-- buttonを押した際にpageの値を変更するイベントを設定 -->
+                <button class="tab" v-on:click="page=2">tab2</button>
+                <button class="tab" v-on:click="page=3">tab3</button>
+            </div>
+            <div class="content" v-bind:class="{ show : page == 1 }"> 
+                <p>page1</p>
+            </div>
+            <!-- pageの値によりclass名が変化（class="content show"）し、cssでshowがついていないタグは表示されない、ついているタグは表示されるように設定している。 -->
+            <div class="content" v-bind:class="{ show : page == 2 }">
+                <p>page2</p>
+            </div>
+            <div class="content" v-bind:class="{ show : page == 3 }">
+                <p>page3</p>
+            </div>            
+        </section>
+
        </main>
 
        <script src="/js/build/top/index.js"></script>
